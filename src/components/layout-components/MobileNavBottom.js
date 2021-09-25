8460c66964000d3da3fa31720eac637d7016811d
Ibrohim <imirxoshimov@gmail.com>
2021-09-25 11:28:40 +0500

    design and active

--- src/components/layout-components/MobileNavBottom.js
+++ src/components/layout-components/MobileNavBottom.js
@@ -1,21 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import Icon from "../util-components/Icon";
 import navigationConfig from "configs/NavigationConfig";
 
-const setLocale = (isLocaleOn, localeKey) => localeKey.toString();
-
-export const MobileNav = ({ localization = true }) => {
-  return (
-    <div className="nav_bottom_menu">
-      {navigationConfig[0].submenu.slice(0, 3).map((menu) => (
-        <Link to={menu.path} className="menu-item" key={menu.key}>
-          {menu.icon ? <Icon type={menu?.icon} /> : null}
-          <span>{setLocale(localization, menu.title)}</span>
-        </Link>
-      ))}
-    </div>
-  );
+export const MobileNav = ({ location }) => {
+	return (
+		<div className="nav_bottom_menu">
+			{navigationConfig[0].submenu.slice(0, 3).map((menu) => (
+				<Link
+					to={menu.path}
+					className={
+						"menu-item" + (location.pathname === menu.path ? " active" : "")
+					}
+					key={menu.key}
+				>
+					{menu.icon ? <Icon type={menu?.icon} /> : null}
+					<span>{menu.title}</span>
+				</Link>
+			))}
+		</div>
+	);
 };
 
-export default MobileNav;
+export default withRouter(MobileNav);
