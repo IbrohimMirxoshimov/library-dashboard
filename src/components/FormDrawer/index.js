98a7e6c14c6f62f70f01a0e634c97de6a7314aa8
Ibrohim <imirxoshimov@gmail.com>
2024-07-18 16:30:19 +0500

    pretty

--- src/components/FormDrawer/index.js
+++ src/components/FormDrawer/index.js
@@ -10,181 +10,203 @@ import DeleteButton from "components/forms/DeleteButton";
 import { isEmptyObject } from "utils/array";
 
 function getInitilaFormData(config, record = {}) {
-	if (config?.formInitial) {
-		return { ...config?.formInitial, ...record };
-	}
+  if (config?.formInitial) {
+    return { ...config?.formInitial, ...record };
+  }
 
-	return record;
+  return record;
 }
 
 function generateFields(fields, user, data, form) {
-	return fields
-		.filter((field) => !field.role || user[field.role])
-		.map((field, i) => {
-			let Component = field.Component;
-			let FieldComponent =
-				field.fieldComponent || FieldComponents[field.field || "input"];
+  return fields
+    .filter((field) => !field.role || user[field.role])
+    .map((field, i) => {
+      let Component = field.Component;
+      let FieldComponent =
+        field.fieldComponent || FieldComponents[field.field || "input"];
 
-			if (Component) {
-				return (
-					<Col xs={24} xl={field.colSpan || 12} key={i}>
-						<Component form={form} user={user} data={data} />
-					</Col>
-				);
-			}
+      if (Component) {
+        return (
+          <Col
+            xs={24}
+            xl={field.colSpan || 12}
+            key={i}
+          >
+            <Component
+              form={form}
+              user={user}
+              data={data}
+            />
+          </Col>
+        );
+      }
 
-			return (
-				<Col xs={24} xl={field.colSpan || 12} key={i}>
-					<Form.Item
-						key={field.name + (data?.id || "f")}
-						name={!field.sub && field.name}
-						valuePropName={field.propName || "value"}
-						label={field.label || field.name}
-						hidden={!data.edit && field.hidden}
-						rules={field.rules}
-					>
-						{field.sub ? (
-							generateFields(field.sub, user, data, form)
-						) : (
-							<FieldComponent
-								key={field.name + (data?.id || "f")}
-								disabled={field.disabledOnEdit && data.id ? true : false}
-								{...field.fieldProp}
-							/>
-						)}
-					</Form.Item>
-				</Col>
-			);
-		});
+      return (
+        <Col
+          xs={24}
+          xl={field.colSpan || 12}
+          key={i}
+        >
+          <Form.Item
+            key={field.name + (data?.id || "f")}
+            name={!field.sub && field.name}
+            valuePropName={field.propName || "value"}
+            label={field.label || field.name}
+            hidden={!data.edit && field.hidden}
+            rules={field.rules}
+          >
+            {field.sub ? (
+              generateFields(field.sub, user, data, form)
+            ) : (
+              <FieldComponent
+                key={field.name + (data?.id || "f")}
+                disabled={field.disabledOnEdit && data.id ? true : false}
+                {...field.fieldProp}
+              />
+            )}
+          </Form.Item>
+        </Col>
+      );
+    });
 }
 
 function FormDrawer() {
-	const { messageId, data = {} } = useMessage("f_d");
+  const { messageId, data = {} } = useMessage("f_d");
 
-	return <FormDrawerMicro messageId={messageId} data={data} />;
+  return (
+    <FormDrawerMicro
+      messageId={messageId}
+      data={data}
+    />
+  );
 }
 
 export function FormDrawerMicro({ messageId, data, onFormClose }) {
-	const user = useSelector((state) => state.auth.user);
+  const user = useSelector((state) => state.auth.user);
 
-	const [visible, setVisible] = useState(false);
-	const [loading, setLoading] = useState(false);
-	const endpoint = data.form;
-	const resource = data.resource;
-	const [form] = useForm();
+  const [visible, setVisible] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const endpoint = data.form;
+  const resource = data.resource;
+  const [form] = useForm();
 
-	useEffect(() => {
-		if (messageId && !visible) {
-			setVisible(true);
-			form.setFieldsValue(data.record || {});
-		}
-		// eslint-disable-next-line
-	}, [messageId]);
+  useEffect(() => {
+    if (messageId && !visible) {
+      setVisible(true);
+      form.setFieldsValue(data.record || {});
+    }
+    // eslint-disable-next-line
+  }, [messageId]);
 
-	function onClose(response) {
-		setVisible(false);
-		form.resetFields();
-		onFormClose && onFormClose(response);
-	}
+  function onClose(response) {
+    setVisible(false);
+    form.resetFields();
+    onFormClose && onFormClose(response);
+  }
 
-	function fetch(values) {
-		if (data.record?.id) {
-			return FetchResource.update(endpoint, data.record.id, values);
-		}
+  function fetch(values) {
+    if (data.record?.id) {
+      return FetchResource.update(endpoint, data.record.id, values);
+    }
 
-		return FetchResource.create(endpoint, values);
-	}
+    return FetchResource.create(endpoint, values);
+  }
 
-	function onFinish(values) {
-		setLoading(true);
-		if (isEmptyObject(values.address)) {
-			values.address = undefined;
-		}
-		fetch(values)
-			.then((res) => {
-				message.success("Success");
-				setLoading(false);
-				onClose(res);
-				window.refreshList && window.refreshList();
-			})
-			.catch((err) => {
-				setLoading(false);
-				message.error(
-					err.response?.data?.message ||
-						err.response?.data?.errors.message ||
-						err.message
-				);
-			});
-	}
+  function onFinish(values) {
+    setLoading(true);
+    if (isEmptyObject(values.address)) {
+      values.address = undefined;
+    }
+    fetch(values)
+      .then((res) => {
+        message.success("Success");
+        setLoading(false);
+        onClose(res);
+        window.refreshList && window.refreshList();
+      })
+      .catch((err) => {
+        setLoading(false);
+        message.error(
+          err.response?.data?.message ||
+            err.response?.data?.errors.message ||
+            err.message
+        );
+      });
+  }
 
-	if (!data.form) return "";
+  if (!data.form) return "";
 
-	const config = pagesConfig[data.form];
-	// console.log("data", data);
-	const Custom = config.view?.Custom;
-	// console.log(config.view);
-	return (
-		<div>
-			<Drawer
-				key={messageId}
-				title={resource.nameOne}
-				placement="right"
-				onClose={onClose}
-				open={visible}
-				footer={
-					<div
-						key={messageId}
-						className="d-flex justify-content-between overflow-auto"
-					>
-						<Button
-							disabled={loading}
-							onClick={onClose}
-							style={{ marginRight: 8 }}
-						>
-							Orqaga
-						</Button>
-						{data.id &&
-						config.view?.canDelete &&
-						config.view.canDelete(data.record) ? (
-							<DeleteButton
-								{...data}
-								onClose={onClose}
-								setLoading={setLoading}
-								loading={loading}
-							/>
-						) : (
-							""
-						)}
-						{config.footer && config.footer(data, form)}
-						<Button
-							loading={loading}
-							disabled={loading}
-							htmlType="submit"
-							form={"mf-" + endpoint}
-							type="primary"
-						>
-							Saqlash
-						</Button>
-					</div>
-				}
-				destroyOnClose
-			>
-				<Form
-					form={form}
-					// key={data?.id || "f"}
-					layout="vertical"
-					initialValues={getInitilaFormData(config, data.record)}
-					id={"mf-" + endpoint}
-					onFinish={onFinish}
-				>
-					<Row gutter={6}>{generateFields(config.form, user, data, form)}</Row>
-				</Form>
-				{data.id && Custom && (
-					<Custom {...data} onClose={onClose} form={form} user={user} />
-				)}
-			</Drawer>
-		</div>
-	);
+  const config = pagesConfig[data.form];
+  // console.log("data", data);
+  const Custom = config.view?.Custom;
+  // console.log(config.view);
+  return (
+    <div>
+      <Drawer
+        key={messageId}
+        title={resource.nameOne}
+        placement="right"
+        onClose={onClose}
+        open={visible}
+        footer={
+          <div
+            key={messageId}
+            className="d-flex justify-content-between overflow-auto"
+          >
+            <Button
+              disabled={loading}
+              onClick={onClose}
+              style={{ marginRight: 8 }}
+            >
+              Orqaga
+            </Button>
+            {data.id &&
+            config.view?.canDelete &&
+            config.view.canDelete(data.record) ? (
+              <DeleteButton
+                {...data}
+                onClose={onClose}
+                setLoading={setLoading}
+                loading={loading}
+              />
+            ) : (
+              ""
+            )}
+            {config.footer && config.footer(data, form)}
+            <Button
+              loading={loading}
+              disabled={loading}
+              htmlType="submit"
+              form={"mf-" + endpoint}
+              type="primary"
+            >
+              Saqlash
+            </Button>
+          </div>
+        }
+        destroyOnClose
+      >
+        <Form
+          form={form}
+          // key={data?.id || "f"}
+          layout="vertical"
+          initialValues={getInitilaFormData(config, data.record)}
+          id={"mf-" + endpoint}
+          onFinish={onFinish}
+        >
+          <Row gutter={6}>{generateFields(config.form, user, data, form)}</Row>
+        </Form>
+        {data.id && Custom && (
+          <Custom
+            {...data}
+            onClose={onClose}
+            form={form}
+            user={user}
+          />
+        )}
+      </Drawer>
+    </div>
+  );
 }
 
 export default FormDrawer;
