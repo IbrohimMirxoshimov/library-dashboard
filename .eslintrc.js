7d10bccc73cb184a1f69bf0b0931d34dce864b9d
Curious <curious.startpro@gmail.com>
2024-11-24 12:27:54 +0500

    chore: refactor

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   root: true,
   env: { browser: true, es2020: true },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
@@ -67,4 +67,4 @@ module.exports = {
       version: 'detect',
     },
   },
-};
+}
